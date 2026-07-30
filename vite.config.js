1b9b9493da9e00caf05e49dc6fbb362941295db5
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2026-07-30 11:18:03 +0300

    Bug fix;

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/map-gen-ai/',
+  base: '/VibedMaps/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
